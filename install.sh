5ea5ad622f26c2802bc505b139ae250e1287f186
Nam Nguyen <ngd.nam283@gmail.com>
2024-12-10 09:36:09 +0700

    no venv in install.sh

--- install.sh
+++ install.sh
@@ -10,22 +10,14 @@ sudo apt-get update
 echo "Step 2: Installing Python3 and pip"
 sudo apt-get install -y python3 python3-pip
 
-echo "Step 3: Installing virtual environment module"
-sudo apt-get install -y python3-venv
-
-echo "Step 4: Setting up virtual environment"
-python3 -m venv .venv
-source .venv/bin/activate
-
-echo "Step 5: Installing dependencies (if requirements.txt exists)"
+echo "Step 3: Installing dependencies (if requirements.txt exists)"
 if [ -f "requirements.txt" ]; then
-    pip install -r requirements.txt
+    pip3 install -r requirements.txt
 else
     echo "No requirements.txt found, skipping dependencies installation."
 fi
 
-echo "Step 6: Running the Python script"
-
+echo "Step 4: Running the Python script"
 nohup python3 "$SCRIPT_NAME" &
 
 echo "Setup complete!"
