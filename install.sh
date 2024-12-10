3d592d878c2ec7d6f6e63fdb332a3fbb228054ca
Nam Nguyen <ngd.nam283@gmail.com>
2024-12-10 09:38:32 +0700

    add venv

--- install.sh
+++ install.sh
@@ -8,16 +8,22 @@ echo "Step 1: Updating package lists"
 sudo apt-get update
 
 echo "Step 2: Installing Python3 and pip"
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip python3-venv
 
-echo "Step 3: Installing dependencies (if requirements.txt exists)"
+echo "Step 3: Setting up virtual environment"
+python3 -m venv .venv
+
+echo "Step 4: Activating virtual environment"
+source .venv/bin/activate
+
+echo "Step 5: Installing dependencies (if requirements.txt exists)"
 if [ -f "requirements.txt" ]; then
-    pip3 install -r requirements.txt
+    pip install -r requirements.txt
 else
     echo "No requirements.txt found, skipping dependencies installation."
 fi
 
-echo "Step 4: Running the Python script"
+echo "Step 6: Running the Python script"
 nohup python3 "$SCRIPT_NAME" &
 
 echo "Setup complete!"
